880c27fdd19e2d8782eb59d0c522b98a61eff21b
T0HAD <ap97824@gmail.com>
2025-01-20 12:20:56 +0300

    доработал программу. но стоит протестировать все случаи программы

--- MuravevGoman222/MainWindow.xaml.cs
+++ MuravevGoman222/MainWindow.xaml.cs
@@ -20,9 +20,57 @@ namespace MuravevGoman222
     /// </summary>
     public partial class MainWindow : Window
     {
+        double x, b, xb, function, result;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonStart(object sender, RoutedEventArgs e)
+        {
+            if (!double.TryParse(InputX.Text, out x) || !double.TryParse(InputB.Text, out b))
+            {
+                MessageBox.Show("Неправильные значения x или b!");
+                return;
+            }
+            xb = x * b;
+            if (Fx.IsChecked == true)
+            {
+                function = Math.Pow(x, 2);
+            }
+            else if (Fe.IsChecked == true)
+            {
+                function = Math.Pow(Math.E, x);
+            }
+            else if (Fs.IsChecked == true)
+            {
+                function = Math.Sinh(x);
+            }
+            else
+            {
+                MessageBox.Show("Выберите функцию!");
+                return;
+            }
+
+            if (xb > 1 && xb < 10)
+            {
+                result = Math.Pow(Math.E, function);
+            }
+            else if (xb > 12 && xb < 40)
+            {
+                result = Math.Sqrt(Math.Abs(function + 4 * b));
+            }
+            else
+            {
+                result = b * Math.Pow(function, 2);
+            }
+            OutputResult.Text = result.ToString();
+        }
+
+        private void ButtonReset(object sender, RoutedEventArgs e)
+        {
+            InputX.Text = ""; InputB.Text = ""; OutputResult.Text = "";
+            Fx.IsChecked = false; Fe.IsChecked = false; Fs.IsChecked = false;
         }
     }
 }
